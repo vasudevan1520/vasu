120324357809e8c615535cce45b3bf77d92067ff
vasudevan1520 <testmailvasudevan@gmail.com>
2022-07-09 15:45:31 +0530

    hai

---
+++ src/com/overriding/IndianBank2021.java
@@ -0,0 +1,16 @@
+package com.overriding;
+
+public class IndianBank2021 {
+	public void interset() {
+		System.out.println("interset 20");
+		this.saving();
+	}
+	public void saving() {
+		System.out.println("saving 40");
+		
+
+	}
+
+	
+
+}
---
+++ src/com/overriding/IndianBank2022.java
@@ -0,0 +1,25 @@
+package com.overriding;
+
+public class IndianBank2022 extends IndianBank2021{
+	
+	@Override
+	public void interset() {
+		
+		super.interset();
+		System.out.println("interset 45");
+		this.deposite();
+	}
+	public void deposite() {
+		System.out.println("deposite 50000");
+		
+
+	}
+
+	public static void main(String[] args) {
+		IndianBank2022 ind = new IndianBank2022();
+		ind.interset();
+		
+
+	}
+
+}
